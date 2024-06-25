ebc0e9be855732bb90b0fc60c04e74a8f9ffe1ce
JoeyMCPC <142174364+JoeyMCPC@users.noreply.github.com>
2024-06-25 16:20:41 -0400

    Create class_core.cpp
    init all of the main classes

---
+++ class_core.cpp
@@ -0,0 +1,21 @@
+#include "class_core.h"
+
+class fighter;
+
+fighter.health = 30;
+fighter.damage = 15;
+fighter.ac = 0;
+
+
+class wizard;
+
+wizard.health = 15;
+wizard.damage = 30;
+wizard.ac = 0;
+
+
+class ranger;
+
+ranger.health = 15;
+ranger.damage = 15;
+ranger.ac = 30;
