92706b27f4d41c159992e52b27a1361a217c24b6
JoeyMCPC <142174364+JoeyMCPC@users.noreply.github.com>
2024-06-25 16:18:19 -0400

    Add files via upload
    class_core.h contains the code for the main classes in the game

---
+++ class_core.h
@@ -0,0 +1,35 @@
+#include <iostream>
+
+/*
+
+rpg test classes
+
+*/
+
+int j = get_srand_seed();
+
+
+
+class class 
+{
+public:
+
+    int health;
+    int damage;
+    int ac;
+
+
+
+    bool attack(int enemy_ac, int j)
+    {
+        if (j > enemy_ac) 
+        {
+            return true;
+        }
+        else
+        {return false;}
+
+        return NULL;
+    }
+
+}
